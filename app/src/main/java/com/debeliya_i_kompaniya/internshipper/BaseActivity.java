3cd0c0a78327e79c530e37923909ce4f0075f41b
Stoyan-Ivanov <stoyaniviv@gmail.com>
2018-03-16 11:03:47 +0200

    Adding bottom navigation

--- app/src/main/java/com/debeliya_i_kompaniya/internshipper/BaseActivity.java
+++ app/src/main/java/com/debeliya_i_kompaniya/internshipper/BaseActivity.java
@@ -10,7 +10,6 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
     }
 
     @Override
