44539558d95d851557fc62571cca30fd9f23a32c
Stoyan-Ivanov <stoyaniviv@gmail.com>
2018-03-16 09:59:17 +0200

    no message

--- app/src/main/java/com/debeliya_i_kompaniya/internshipper/LoginActivity.java
+++ app/src/main/java/com/debeliya_i_kompaniya/internshipper/LoginActivity.java
@@ -3,7 +3,7 @@ package com.debeliya_i_kompaniya.internshipper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
