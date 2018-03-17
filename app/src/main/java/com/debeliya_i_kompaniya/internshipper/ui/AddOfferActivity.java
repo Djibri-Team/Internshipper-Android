dc133a68cd46a3d2a498bc9276cab909368d2aa7
Stoyan-Ivanov <stoyaniviv@gmail.com>
2018-03-17 15:33:30 +0200

    Adding fix to screen transitioning

--- app/src/main/java/com/debeliya_i_kompaniya/internshipper/ui/AddOfferActivity.java
+++ app/src/main/java/com/debeliya_i_kompaniya/internshipper/ui/AddOfferActivity.java
@@ -2,11 +2,13 @@ package com.debeliya_i_kompaniya.internshipper.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 
 import com.dd.CircularProgressButton;
 import com.debeliya_i_kompaniya.internshipper.R;
+import com.debeliya_i_kompaniya.internshipper.constants.BottomNavOptions;
 import com.debeliya_i_kompaniya.internshipper.ui.base_activities.BaseActivity;
 
 import butterknife.BindView;
@@ -21,15 +23,18 @@ public class AddOfferActivity extends BaseActivity {
     @BindView(R.id.rb_software) RadioButton rbSoftware;
     @BindView(R.id.rb_hardware) RadioButton rbHardware;
     @BindView(R.id.rb_embedded) RadioButton rbEmbedded;
-    @BindView(R.id.btn_add_offer) CircularProgressButton btnAddOffer;
+    @BindView(R.id.btn_add_offer) Button btnAddOffer;
 
     @OnClick(R.id.btn_add_offer)
     void addOffer() {
         checkIfFieldsAreEmpty();
+
+        startActivity(AllOffers.getIntent(this, BottomNavOptions.OFFERLIST));
+        finish();
     }
 
     private void checkIfFieldsAreEmpty() {
-        
+
     }
 
     @Override
